62a21ca40e20fa57af7336b81fe39733f1a822dc
Wagner Bruna <wbruna@users.noreply.github.com>
2025-10-08 18:45:48 -0300

    default to empty string on the seed parameter
    Make it display the message "Enter seed here" by default,
    instead of -1 (which is still accepted if provided
    manually).

--- src/stores/generator.ts
+++ src/stores/generator.ts
@@ -18,7 +18,7 @@ function getDefaultStore() {
         height: 512, // make sure these are divisible by 64
         cfg_scale: 6,
         clip_skip: 0,
-        seed: -1,
+        seed: "",
         denoising_strength: 0.6,
         frames: 1,
     }
